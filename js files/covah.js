5455f36640c3d1d4a3d924357422b3ff7decb93d
najla <najlavahdani123@gmail.com>
2023-07-29 13:15:53 +0430

    filters and products section responsived

--- js files/covah.js
+++ js files/covah.js
@@ -99,3 +99,21 @@ var specialSwiper = new Swiper(".special-swiper", {
   });
 //special swiper ends
 
+//search section starts
+let searchForm = document.querySelector('.search-form')
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+//search section ends
+
