e65269ae999b53cd7f048b5d4ab78cf911836b02
najla <najlavahdani123@gmail.com>
2023-08-25 10:31:43 +0330

    drop down list and filters section edited

--- js files/products.js
+++ js files/products.js
@@ -17,3 +17,42 @@ filterBoxes.forEach(function(filterBox) {
 });
 
 
+
+
+//header-3 and dropdown list on scroll section starts
+window.onscroll = () => {
+  if(window.scrollY > 80){
+      document.querySelector('.header .header-3').classList.add('active');
+
+
+      dropdown.classList.remove('active')
+      
+      document.querySelector('#dropdown-list-btn').onclick =() =>{
+          dropdown.classList.toggle('active-2')
+      }
+
+      document.querySelector('#close-categories').onclick=() =>{
+          dropdown.classList.remove('active-2')
+      }
+  }
+  else{
+      document.querySelector('.header .header-3').classList.remove('active');
+      
+
+      dropdown.classList.remove('active-2')
+
+      document.querySelector('#dropdown-list-btn').onclick =() =>{
+          dropdown.classList.toggle('active')
+      }
+      
+      document.querySelector('#close-categories').onclick=() =>{
+          dropdown.classList.remove('active')
+      }
+      
+  }
+
+  
+}
+//header-3 and dropdown list on scroll section ends
+
+
